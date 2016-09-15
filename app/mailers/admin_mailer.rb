0fb9a05e6f5f146babeb45aa3e3d3016c1621298
Andres <andresg123@gmail.com>
2016-09-15 18:44:25 -0300

    Se modifica el mail de nuevo usuario para el admin

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,6 +2,6 @@ class AdminMailer < ApplicationMailer
   def new_user_mail(admin_user)
     @user = admin_user
     @url  = admin_users_url
-    mail(to: @user.email, subject: 'Nuevo usuario en el sistema')
+    mail(to: @user.email, subject: 'Nuevo usuario en ASH-WEB')
   end
 end
