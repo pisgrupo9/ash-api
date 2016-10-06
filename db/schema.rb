1cfdb8960b699baa22db5a8b60d334d340d98609
lubarrero <luciab.91@gmail.com>
2016-10-05 22:14:27 -0300

    busqueda de eventos

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160929221735) do
+ActiveRecord::Schema.define(version: 20161005232356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "unaccent"
 
   create_table "admin_users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
