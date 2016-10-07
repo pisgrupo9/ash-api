0f2bc13fe99398f1d4d741733a93ee06aa1ee5f1
Isa14 <isabel@neocoast.com>
2016-10-07 18:59:05 -0300

    Tests de los métodos para el controllador eventos

--- spec/factories/animals.rb
+++ spec/factories/animals.rb
@@ -28,11 +28,19 @@ FactoryGirl.define do
     name { Faker::Name.first_name }
     chip_num { Faker::Code.isbn }
     race { Faker::Name.first_name }
-    sex "female"
+    sex { Faker::Number.between(0, 1) }
     vaccines { Faker::Boolean.boolean }
     castrated { Faker::Boolean.boolean }
-    admission_date { Faker::Time.between(3.days.ago, Date.today, :all) }
-    birthdate { Faker::Time.between(5.days.ago, Date.today, :all) }
+    admission_date { Faker::Date.between(3.days.ago, Date.today) }
+    birthdate { Faker::Date.between(1.year.ago, 4.days.ago) }
+    profile_image { Faker::Internet.url }
+    weight { Faker::Number.number(2) }
     species
+
+    factory :animal_con_evento do
+      after(:create) do |animal|
+        create(:events, animal: animal)
+      end
+    end
   end
 end
