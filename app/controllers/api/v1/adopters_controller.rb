b9f5a58a4b7ae5a097cdd89b947ee8263c2b1bce
Andres <andresg123@gmail.com>
2016-10-15 17:57:39 -0300

    Se agregan los filtros para adoptantes.

--- app/controllers/api/v1/adopters_controller.rb
+++ app/controllers/api/v1/adopters_controller.rb
@@ -36,6 +36,11 @@ module Api
         head :no_content
       end
 
+      def search
+        @adopters = Adopter.search(adopters_search_params).page(params[:page]).per(params[:row])
+        render 'index.json.jbuilder'
+      end
+
       private
 
       def set_adopter
@@ -45,6 +50,10 @@ module Api
       def adopter_params
         params.require(:adopter).permit(:first_name, :last_name, :ci, :email,
                                         :phone, :house_description, :blacklisted, :home_address)
+      end
+
+      def adopters_search_params
+        params.permit(:name, :blacklisted)
       end
     end
   end
