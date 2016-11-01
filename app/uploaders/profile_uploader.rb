e046a714acb7b6675f40367afd8da86342a4d4ad
Bernabe Gonzalez <bernabe@toptierlabs.com>
2016-10-31 23:28:04 -0300

    las imagenes son orientadas segun la metadata EXIF

--- app/uploaders/profile_uploader.rb
+++ app/uploaders/profile_uploader.rb
@@ -7,6 +7,14 @@ class ProfileUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{model.id}/#{mounted_as}"
   end
 
+  def auto_orient
+    manipulate! do |image|
+      image.tap(&:auto_orient!)
+    end
+  end
+
+  process :auto_orient
+
   version :thumb do
     process resize_to_fill: [150, 150]
   end
